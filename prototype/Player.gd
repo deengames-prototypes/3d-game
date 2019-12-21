1bd97470105eba58474efce6583e5130d358561c
nightblade9 <nightbladecodes@gmail.com>
2019-12-20 22:46:33 -0500

    First cut of walking around on planet. Wonky.

--- prototype/Player.gd
+++ prototype/Player.gd
@@ -1,7 +1,9 @@
 extends KinematicBody
 
 # Simple, linear grvity
-var GRAVITY = Vector3(0, -98, 0)
+const GRAVITY = -98
+const MOVE_SPEED = 10
+
 var _velocity = Vector3(0, 0, 0)
 
 # Called when the node enters the scene tree for the first time.
@@ -9,4 +11,23 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	self.move_and_slide(_velocity + GRAVITY)
+	_velocity = Vector3(0, 0, 0)
+	if Input.is_action_pressed("ui_up"):
+		_velocity.z = -MOVE_SPEED
+	elif Input.is_action_pressed("ui_down"):
+		_velocity.z = MOVE_SPEED
+	else:
+		_velocity.z = 0
+	
+	if Input.is_action_pressed("ui_left"):
+		_velocity.x = -MOVE_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		_velocity.x = MOVE_SPEED
+	else:
+		_velocity.x = 0
+
+func _physics_process(delta):
+	var gravity_to_center = self.translation.normalized() * GRAVITY
+	print("G="+str(gravity_to_center))
+	self.move_and_slide(_velocity + gravity_to_center)
+	
