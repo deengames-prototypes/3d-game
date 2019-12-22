c41bb1d56903928be27f8170d5973c64cfca10c4
nightblade9 <nightbladecodes@gmail.com>
2019-12-21 22:49:18 -0500

    Hmm, it actually works! Can sculpt stuff out of blocks!

---
+++ prototypes/voxel-craft/ExpandableCube.gd
@@ -0,0 +1,11 @@
+extends MeshInstance
+
+var ExpandableCube = load("res://ExpandableCube.tscn")
+
+func _on_Area_input_event(camera, event, click_position, click_normal, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		var cube = ExpandableCube.instance()
+		click_position = Vector3(
+			round(click_position.x), round(click_position.y), round(click_position.z))
+		cube.translation = click_position
+		self.get_parent().add_child(cube)
