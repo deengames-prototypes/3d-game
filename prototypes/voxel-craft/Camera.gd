2f0a85281702089f3b075c66d6257f0919a38b55
nightblade9 <nightbladecodes@gmail.com>
2019-12-22 22:24:41 -0500

    Mouse wheel zoom works

--- prototypes/voxel-craft/Camera.gd
+++ prototypes/voxel-craft/Camera.gd
@@ -1,22 +1,22 @@
 extends Camera
 
 const UP_VECTOR:Vector3 = Vector3(0, 1, 0)
-const CAMERA_DISTANCE = 5
 
-var x_angle:int = 0
+var camera_distance = 5
+var z_angle:int = 0
 var y_angle:int = 0
 var target:Spatial
 
 func target_object(obj:Spatial):
 	self.target = obj
 
-func _process(delta):
+func _unhandled_input(event):
 	var moved = false
 	if Input.is_action_pressed("ui_left"):
-		x_angle += 1
+		z_angle += 1
 		moved = true
 	elif Input.is_action_pressed("ui_right"):
-		x_angle -= 1
+		z_angle -= 1
 		moved = true
 		
 	if Input.is_action_pressed("ui_up"):
@@ -26,8 +26,15 @@ func _process(delta):
 		y_angle -= 1
 		moved = true
 	
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_WHEEL_UP:
+		camera_distance -= 1
+		moved = true
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_WHEEL_DOWN:
+		camera_distance += 1
+		moved = true
+	
 	if moved:
-		self.translation.z = CAMERA_DISTANCE * cos(x_angle * PI / 180)
-		self.translation.y = CAMERA_DISTANCE * sin(y_angle * PI / 180)
+		self.translation.z = camera_distance * cos(z_angle * PI / 180)
+		self.translation.y = camera_distance * sin(y_angle * PI / 180)
 		self.look_at(target.translation, UP_VECTOR)
 		
